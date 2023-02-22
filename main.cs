446143d9647c8c092e3b72a7d5dd257a2cea8c4f
Alejandro Minor <AlejandroMinor@outlook.com>
2023-02-22 01:17:03 -0600

    CHANGE Se agregan elementos de diseño

--- main.cs
+++ main.cs
@@ -14,6 +14,7 @@ namespace SoftAssist
 
             SelectedItemImage.BackColor= Color.Transparent;
             SelectedItemImage.Visible= false;
+            selectedForm.Visible = false;
 
             changeImages(x64button, "monitor", 0);
             changeImages(x86button, "monitor", 50);
@@ -27,13 +28,13 @@ namespace SoftAssist
 
         }
         private void changeImages(Button buttonName, String buttonImgName, int ejeY) {
-            // Obtiene la posición actual del botón
+
             Point currentLocation = SelectedItemImage.Location;
 
             buttonName.FlatAppearance.BorderSize = 0;
             buttonName.MouseEnter += (sender, e) =>
             {
-                buttonName.ForeColor = Color.FromArgb(78, 70, 133);
+                buttonName.ForeColor = Color.FromArgb(255, 76, 52);
 
                 string dir = Path.GetDirectoryName(Application.ExecutablePath);
                 string nombreArchivo = Path.Combine(dir, $@"img\icons8-{buttonImgName}-24B.png");
@@ -46,7 +47,7 @@ namespace SoftAssist
 
             buttonName.MouseLeave += (sender, e) =>
             {
-                buttonName.ForeColor = Color.FromArgb(146, 146, 174);
+                buttonName.ForeColor = Color.FromArgb(255, 255, 255);
 
                 string dir = Path.GetDirectoryName(Application.ExecutablePath);
                 string nombreArchivo = Path.Combine(dir, $@"img\icons8-{buttonImgName}-24A.png");
@@ -75,23 +76,151 @@ namespace SoftAssist
             // Mostrar el formulario
             x64form.Show();
 
+            focusButton(x64button);
+
+        }
+
+        private void focusButton(Button buttonID) {
+
+            if (buttonID == x64button)
+            {
+                x64button.Focus();
+                x64button.FlatAppearance.BorderSize = 1;
+
+                selectedForm.Visible = true;
+                selectedForm.Location = new Point(122, 24);
+            }
+            else {
+                x64button.FlatAppearance.BorderSize = 0;
+            }
+            if (buttonID == x86button)
+            {
+                x86button.Focus();
+                x86button.FlatAppearance.BorderSize = 1;
+
+                selectedForm.Visible = true;
+                selectedForm.Location = new Point(122, 76);
+
+            }
+            else { 
+                x86button.FlatAppearance.BorderSize = 0;
+            }
+            if (buttonID == officeSuiteButton)
+            {
+                officeSuiteButton.Focus();
+                officeSuiteButton.FlatAppearance.BorderSize = 1;
+
+                selectedForm.Visible = true;
+                selectedForm.Location = new Point(122, 121);
+            }
+            else
+            {
+                officeSuiteButton.FlatAppearance.BorderSize = 0;
+            }
+            if (buttonID == keysbutton)
+            {
+                keysbutton.Focus();
+                keysbutton.FlatAppearance.BorderSize = 1;
+
+                selectedForm.Visible = true;
+                selectedForm.Location = new Point(122, 167);
+            }
+            else 
+            { 
+                keysbutton.FlatAppearance.BorderSize = 0;
+            }
+            if (buttonID == toolsButton)
+            {
+                toolsButton.Focus();
+                toolsButton.FlatAppearance.BorderSize = 1;
+
+                selectedForm.Visible = true;
+                selectedForm.Location = new Point(122, 213);
+            }
+            else 
+            { 
+                toolsButton.FlatAppearance.BorderSize = 0;
+            }
+            if (buttonID == backupButton)
+            {
+                backupButton.Focus();
+                backupButton.FlatAppearance.BorderSize = 1;
+
+                selectedForm.Visible = true;
+                selectedForm.Location = new Point(122, 259);
+            }
+            else
+            {
+                backupButton.FlatAppearance.BorderSize = 0;
+            }
+            if (buttonID == systemInfoButton)
+            {
+                systemInfoButton.Focus();
+                systemInfoButton.FlatAppearance.BorderSize = 1;
+
+                selectedForm.Visible = true;
+                selectedForm.Location = new Point(122, 305);
+            }
+            else
+            {
+                systemInfoButton.FlatAppearance.BorderSize = 0;
+            }
+            if (buttonID == logButton)
+            {
+                logButton.Focus();
+                logButton.FlatAppearance.BorderSize = 1;
+
+                selectedForm.Visible = true;
+                selectedForm.Location = new Point(122, 351);
+            }
+            else
+            {
+                logButton.FlatAppearance.BorderSize = 0;
+            }
+
 
 
         }
 
+
         private void officeSuiteButton_Click(object sender, EventArgs e)
         {
-
+            focusButton(officeSuiteButton);
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+            focusButton(keysbutton);
         }
 
         private void panel1_Paint(object sender, PaintEventArgs e)
         {
 
+        }
+
+        private void x86button_Click(object sender, EventArgs e)
+        {
+            focusButton(x86button);
+        }
+
+        private void toolsButton_Click(object sender, EventArgs e)
+        {
+            focusButton(toolsButton);
+        }
+
+        private void backupButton_Click(object sender, EventArgs e)
+        {
+            focusButton(backupButton);
+        }
+
+        private void systemInfoButton_Click(object sender, EventArgs e)
+        {
+            focusButton(systemInfoButton);
+        }
+
+        private void logButton_Click(object sender, EventArgs e)
+        {
+            focusButton(logButton);
         }
     }
 }
