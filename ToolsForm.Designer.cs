a83e324b83d6e17df33e578388481c1af759dcf7
Alejandro Minor <AlejandroMinor@outlook.com>
2023-03-07 18:03:29 -0600

    [ADD] Funcion para verificar salud de unidades de almacenamiento

--- ToolsForm.Designer.cs
+++ ToolsForm.Designer.cs
@@ -28,19 +28,38 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.crystalDiskradioButton = new System.Windows.Forms.RadioButton();
             this.SuspendLayout();
+            // 
+            // crystalDiskradioButton
+            // 
+            this.crystalDiskradioButton.AutoSize = true;
+            this.crystalDiskradioButton.Location = new System.Drawing.Point(21, 22);
+            this.crystalDiskradioButton.Name = "crystalDiskradioButton";
+            this.crystalDiskradioButton.Size = new System.Drawing.Size(181, 19);
+            this.crystalDiskradioButton.TabIndex = 0;
+            this.crystalDiskradioButton.TabStop = true;
+            this.crystalDiskradioButton.Text = "Salud HDD (Crystal Diskradio)";
+            this.crystalDiskradioButton.UseVisualStyleBackColor = true;
+            this.crystalDiskradioButton.CheckedChanged += new System.EventHandler(this.crystalDiskradioButton_CheckedChanged);
             // 
             // ToolsForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(6)))), ((int)(((byte)(125)))), ((int)(((byte)(189)))));
             this.ClientSize = new System.Drawing.Size(397, 281);
+            this.Controls.Add(this.crystalDiskradioButton);
             this.Name = "ToolsForm";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private RadioButton crystalDiskradioButton;
     }
 }
